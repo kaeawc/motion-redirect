bf39dc382cf8d50f4d5c99862c1d832f052b044a
Jason Pearson <jason.d.pearson@gmail.com>
2019-05-16 10:25:43 -0400

    setup application

---
+++ app/src/main/java/io/kaeawc/motionredirect/App.kt
@@ -0,0 +1,12 @@
+package io.kaeawc.motionredirect
+
+import android.app.Application
+import timber.log.Timber
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
